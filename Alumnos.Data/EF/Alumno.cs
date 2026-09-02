a2edf6d65714248b8ef800fa62442bd238b88bb4
Alex Nakagama <alexnakagama4@gmail.com>
2026-09-01 22:53:25 -0300

    practice scaffold

---
+++ Alumnos.Data/EF/Alumno.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Alumnos.Data.EF;
+
+public partial class Alumno
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string Surname { get; set; } = null!;
+
+    public int Age { get; set; }
+}
---
+++ Alumnos.Data/EF/AlumnosDBContext.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Alumnos.Data.EF;
+
+public partial class AlumnosDBContext : DbContext
+{
+    public AlumnosDBContext(DbContextOptions<AlumnosDBContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Alumno> Alumnos { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder
+            .UseCollation("utf8mb4_0900_ai_ci")
+            .HasCharSet("utf8mb4");
+
+        modelBuilder.Entity<Alumno>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PRIMARY");
+
+            entity.ToTable("alumno");
+
+            entity.Property(e => e.Id)
+                .ValueGeneratedNever()
+                .HasColumnName("id");
+            entity.Property(e => e.Age).HasColumnName("age");
+            entity.Property(e => e.Name)
+                .HasMaxLength(45)
+                .HasColumnName("name");
+            entity.Property(e => e.Surname)
+                .HasMaxLength(45)
+                .HasColumnName("surname");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
