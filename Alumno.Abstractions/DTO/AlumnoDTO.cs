a919ca13f6243e264234fc7e299f10bd11abb23a
Alex Nakagama <alexnakagama4@gmail.com>
2026-09-01 23:45:56 -0300

    created dto for mapping

---
+++ Alumno.Abstractions/DTO/AlumnoDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Alumno.Abstractions.DTO
+{
+    public class AlumnoDTO
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+    }
+}
---
+++ Alumnos.Data/Repositories/AlumnoRepository.cs
@@ -0,0 +1,29 @@
+﻿using Alumno.Abstractions.DTO;
+using Alumnos.Data.EF;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alumnos.Data.Repositories
+{
+    public class AlumnoRepository(AlumnosDBContext _context)
+    {
+        public async Task <List<AlumnoDTO>> GetAllAlumnos()
+        {
+            var alumnos = await (
+                from a in _context.Alumnos
+                select new AlumnoDTO
+                {
+                    Name = a.Name,
+                    Surname = a.Surname,
+                    Age = a.Age,
+                }
+                ).ToListAsync();
+
+            return alumnos;
+        }
+    }
+}
